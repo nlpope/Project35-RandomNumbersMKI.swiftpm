509b8887afd2a1fe61b80af371352b6f04a8a725
Noah Pope <npope@tutanota.com>
2026-02-03 05:51:00 -0600

    adding GKMersenneTwisterRandomSource method example

--- MyApp.swift
+++ MyApp.swift
@@ -20,4 +20,13 @@ func testFunc()
     print(GKRandomSource.sharedRandom().nextInt(upperBound: 6))
     //random bool
     print(GKRandomSource.sharedRandom().nextBool())
+    //random floating point val btwn 0 and 1
+    print(GKRandomSource.sharedRandom().nextUniform())
+    
+    //gameplay kits arc4RandomSource duplicate (predictable, not truly random) spits out rando btwn 0 & 20
+    let arc4 = GKARC4RandomSource()
+    print(arc4.nextInt(upperBound: 20))
+    //more complex and more random number btwn 0 & 20
+    let mersenne = GKMersenneTwisterRandomSource()
+    print(mersenne.nextInt(upperBound: 20))
 }
